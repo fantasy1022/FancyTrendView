8c7a6a3e5e36b812fdbe0966eac15a7fc5f254fc
fantasy1022 <fantasy-1022@hotmail.com>
2019-01-02 21:25:42 +0800

    Change component and scope to Kotlin.

---
+++ app/src/main/java/com/fantasy1022/fancytrendapp/AppComponent.kt
@@ -0,0 +1,18 @@
+package com.fantasy1022.fancytrendapp
+
+import com.fantasy1022.fancytrendapp.injection.FancyTrendComponent
+import com.fantasy1022.fancytrendapp.injection.FancyTrendPresenterModule
+import com.fantasy1022.fancytrendapp.injection.NetworkModule
+
+import javax.inject.Singleton
+
+import dagger.Component
+
+/**
+ * Created by fantasy_apple on 2017/6/25.
+ */
+@Singleton
+@Component(modules = [NetworkModule::class])
+interface AppComponent {
+    operator fun plus(fancyTrendPresenterModule: FancyTrendPresenterModule): FancyTrendComponent
+}
